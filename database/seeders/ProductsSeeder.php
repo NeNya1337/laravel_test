1481ef8331c33e1d8761d6b1168b048e6bc444a5
Mark Lösche <mloesche@bbw-fi.de>
2022-01-17 20:58:08 +0100

    added better DB-Seed

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -15,16 +15,55 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        //Product::factory()->count(10)->make();
-        $i = 1;
-        while($i <= 10){
-            DB::table('products')->insert([
-                'article_number' => rand(1000000, 9999999),
-                'name' => Str::random(10),
-                'description' => Str::random(10),
-                'price' => rand(100, 100000)/100
-            ]);
-            $i++;
-        }
+        DB::table('products')->insert([
+            'article_number' => 10001,
+            'name' => 'Besen',
+            'description' => 'Besen mit langem Stiel und weißen Borsten',
+            'price' => 9.99,
+            'image' => '/images/besen.jpg'
+        ]);
+        DB::table('products')->insert([
+            'article_number' => 10002,
+            'name' => 'Messer',
+            'description' => 'Großes Messer mit scharfer Klinge',
+            'price' => 39.99,
+            'image' => '/images/messer.jpg'
+        ]);
+        DB::table('products')->insert([
+            'article_number' => 10003,
+            'name' => 'Rasenmäher',
+            'description' => 'Elektrischer Rasenmäher mit Fangkorb',
+            'price' => 199.99,
+            'image' => '/images/rasenmaeher.jpg'
+        ]);
+        DB::table('products')->insert([
+            'article_number' => 10004,
+            'name' => 'Schaufel',
+            'description' => 'Kleine Schaufel für große und kleine Gärtner',
+            'price' => 4.99,
+            'image' => '/images/schaufel.jpg'
+        ]);
+        DB::table('products')->insert([
+            'article_number' => 10005,
+            'name' => 'Schaufel und Besen',
+            'description' => 'Set bestehend aus Besen und Kehrschaufel. Hier entkommt kein Schmutz.',
+            'price' => 29.99,
+            'image' => '/images/schaufel_besen.jpg'
+        ]);
+        DB::table('products')->insert([
+            'article_number' => 10006,
+            'name' => 'Schere',
+            'description' => 'Schneidet einfach alles. Außer Tiernahrung.',
+            'price' => 19.99,
+            'image' => '/images/schere.jpg'
+        ]);
+        DB::table('products')->insert([
+            'article_number' => 10007,
+            'name' => 'Gartengeräte-Set',
+            'description' => 'Set bestehend aus verschiedenen kleinen Gartengeräten. auch perfekt für den Balkon geeignet.',
+            'price' => 24.99,
+            'image' => '/images/set.jpg'
+        ]);
+
     }
 }
