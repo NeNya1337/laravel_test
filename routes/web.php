54da682bfff6187d5bfb8e838f6b587b8398ef87
Mark Lösche <mloesche@bbw-fi.de>
2022-01-17 21:28:36 +0100

    changed route logic to controller, added vue

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
-    $productlist = DB::table('products')->get();
-    return view('dashboard',['productlist' => $productlist]);
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/', [\App\Http\Controllers\ProductController::class,'load'])->name('dashboard');
